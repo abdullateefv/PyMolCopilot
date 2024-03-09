67e4c8eff68be2e342affb8c9c33fddb5edb6df0
Arqam Usman Ali <aa358@njit.edu>
2024-03-08 23:16:16 -0500

    Modified code to allow RGB decimal input

--- backend/appearanceFunctions.py
+++ backend/appearanceFunctions.py
@@ -1,18 +1,23 @@
-"""
-Stores functions for manipulating the appearance of PyMol elements
-"""
-
+from enum import Enum
 import json
 
+class Color(Enum):
+    RED = (1.0, 0.0, 0.0)
+    GREEN = (0.0, 1.0, 0.0)
+    BLUE = (0.0, 0.0, 1.0)
+    YELLOW = (1.0, 1.0, 0.0)
+    WHITE = (1.0, 1.0, 1.0)
 
-def bg_color(color):
+def bg_color(color=None, rgb=None):
     """
     Sets the background color
 
     Parameters
     ----------
-    color: str
-        Desired background color
+    color: str, optional
+        Desired background color (default is None)
+    rgb: tuple, optional
+        RGB decimal format (default is None)
 
     Returns
     -------
@@ -23,7 +28,17 @@ def bg_color(color):
     from pymol import cmd
 
     try:
-        cmd.bg_color(color)
-        return json.dumps({"status": "success", "bg_color_set": color})
+        if color is not None:
+            if color in Color.__members__:
+                cmd.bg_color(color)
+            else:
+                raise ValueError("Invalid color name")
+        elif rgb is not None:
+            cmd.set_color("custom_color", rgb)
+            cmd.bg_color("custom_color")
+        else:
+            raise ValueError("Both color name and RGB value are None")
+
+        return json.dumps({"status": "success", "bg_color_set": color if color else "custom_color"})
     except Exception as exceptionMessage:
         return json.dumps({"status": "failed"})
