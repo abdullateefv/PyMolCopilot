c8c206931d2148ee4aa46be366937ab80b5f9b9e
Arqam Usman Ali <aa358@njit.edu>
2024-03-12 01:02:12 -0400

    Omitted unnecessary code and modified error msgs

--- backend/appearanceFunctions.py
+++ backend/appearanceFunctions.py
@@ -1,12 +1,8 @@
-from enum import Enum
-import json
+"""
+Stores functions for manipulating the appearance of PyMol elements
+"""
 
-class Color(Enum):
-    RED = (1.0, 0.0, 0.0)
-    GREEN = (0.0, 1.0, 0.0)
-    BLUE = (0.0, 0.0, 1.0)
-    YELLOW = (1.0, 1.0, 0.0)
-    WHITE = (1.0, 1.0, 1.0)
+import json
 
 def bg_color(color=None, rgb=None):
     """
@@ -16,7 +12,7 @@ def bg_color(color=None, rgb=None):
     ----------
     color: str, optional
         Desired background color (default is None)
-    rgb: tuple, optional
+    rgb: array, optional
         RGB decimal format (default is None)
 
     Returns
@@ -30,14 +26,14 @@ def bg_color(color=None, rgb=None):
     try:
         if color is not None:
             if cmd.get_color_index(color) == -1:
-                raise ValueError("Invalid color name")
+                raise ValueError("Invalid color name, consider using a custom RGB color array instead")
             cmd.bg_color(color)
         elif rgb is not None:
             cmd.set_color("custom_color", rgb)
             cmd.bg_color("custom_color")
         else:
-            raise ValueError("Both color name and RGB value are None")
+            raise ValueError("Must enter either a color name or a RGB value")
 
         return json.dumps({"status": "success", "bg_color_set": color if color else "custom_color"})
     except Exception as exceptionMessage:
-        return json.dumps({"status": "failed"})
+        return json.dumps({"status": "failed", "message": "Given color not identifiable. Unable to change color."})
