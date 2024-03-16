1eb3c4a4cf9594869d2335b9f24c802335312ce0
Arqam Usman Ali <aa358@njit.edu>
2024-03-16 01:18:53 -0400

    Added code to run the protect function

--- backend/appearanceFunctions.py
+++ backend/appearanceFunctions.py
@@ -27,3 +27,26 @@ def bg_color(color):
         return json.dumps({"status": "success", "bg_color_set": color})
     except Exception as exceptionMessage:
         return json.dumps({"status": "failed"})
+
+def protect(selection):
+    """
+    Protects a set of atoms from transformations
+
+    Parameters
+    ----------
+    selection: str
+        Selection of atoms to protect
+
+    Returns
+    -------
+    results : str
+        Result of command execution as JSON formatted string
+    """
+
+    from pymol import cmd
+
+    try:
+        cmd.protect(selection)
+        return json.dumps({"status": "success", "protected_atoms": selection, "message": "Given selection is protected."})
+    except Exception as exceptionMessage:
+        return json.dumps({"status": "failed", "message": "Given selection not identifiable. Unable to protect selection."})
