d258f84783359b458bfc7f0dc375fb2151fbb25e
Abdul Lateef FNU <abdullateefv@gmail.com>
2024-03-21 18:22:57 -0400

    Finished Cartoon Command
    Signed-off-by: Abdul Lateef FNU <abdullateefv@gmail.com>

--- backend/appearanceFunctions.py
+++ backend/appearanceFunctions.py
@@ -3,6 +3,7 @@ Stores functions for manipulating the appearance of PyMol elements
 """
 
 import json
+from pymol import cmd
 
 
 def bg_color(color):
@@ -16,14 +17,39 @@ def bg_color(color):
 
     Returns
     -------
-    results : str
+    response : str
         result of command execution as JSON formatted string
     """
-
-    from pymol import cmd
 
     try:
         cmd.bg_color(color)
         return json.dumps({"status": "success", "bg_color_set": color})
     except Exception as exceptionMessage:
         return json.dumps({"status": "failed"})
+
+
+def cartoon_cmd(type, selection=None):
+    """
+    Sets the cartoon display style
+
+    Parameters
+    ----------
+    type: str
+        The desired cartoon style
+    selection: str
+        Specifies the name of the selection that should have its cartoon style set
+
+    Returns
+    -------
+    response : str
+        result of command execution as JSON formatted string
+    """
+    try:
+        if selection:
+            cmd.cartoon(type, selection)
+            return json.dumps({"status": "success", "type_set": type, "selection_set": selection})
+        else:
+            cmd.cartoon(type)
+            return json.dumps({"status": "success", "type_set": type})
+    except Exception as exceptionMessage:
+        return json.dumps({"status": "failed", "message": exceptionMessage})
