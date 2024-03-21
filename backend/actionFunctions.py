d627e629e749a6a56fbf6056a433d80e96bceb62
Arqam Usman Ali <aa358@njit.edu>
2024-03-21 18:30:09 -0400

    Update actionFunctions.py

--- backend/actionFunctions.py
+++ backend/actionFunctions.py
@@ -2,17 +2,18 @@ import json
 
 def bond(atom1, atom2): 
     """
-    Sets the background color
+   creates a bond between two selections
 
     Parameters
     ----------
-    color: str
-        Desired background color
+    atom1: str
+       The first atom selection
+    atom2: str
+       The second atom selection
 
     Returns
     -------
-    results : str
-        result of command execution as JSON formatted string
+    bonded atom structure
     """
 
     from pymol import cmd 
@@ -20,5 +21,5 @@ def bond(atom1, atom2):
     try:
         cmd.bond(atom1, atom2)
         return json.dumps({"status": "success", "message": "Atoms are bonded"})
-    except Exception as e:
-        return json.dumps({"status": "failed", "message": "Unable to execute the bond command"})
+    except Exception as exceptionMessage:
+        return json.dumps({"status": "failed", "message": exceptionMessage})
